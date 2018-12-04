e24a3fa4c778dc2d7759ab47c31228c4e89e424d
Sam Pettersson <sam@sampettersson.com>
2018-12-04 15:29:21 +0100

    Initial commit

---
+++ CoreSignal+Feedback.swift
@@ -0,0 +1,59 @@
+//
+//  CoreSignal+Feedback.swift
+//  FlowFeedback
+//
+//  Created by Sam Pettersson on 2018-11-30.
+//  Copyright © 2018 Hedvig AB. All rights reserved.
+//
+
+import Flow
+import Foundation
+
+enum FeedbackType {
+    case error, warning, success, selection, impactLight, impactMedium, impactHeavy
+}
+
+private struct Feedback {
+    static func generateNotification(_ type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+
+    static func generateSelection() {
+        let generator = UISelectionFeedbackGenerator()
+        generator.selectionChanged()
+    }
+
+    static func generateImpact(_ style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+}
+
+extension CoreSignal {
+    /// Generate haptic feedback
+    public func feedback(type: FeedbackType) -> Disposable {
+        let bag = DisposeBag()
+
+        bag += onValue { _ in
+            switch type {
+            case .error:
+                Feedback.generateNotification(.error)
+            case .warning:
+                Feedback.generateNotification(.warning)
+            case .success:
+                Feedback.generateNotification(.success)
+            case .selection:
+                Feedback.generateSelection()
+            case .impactLight:
+                Feedback.generateImpact(.light)
+            case .impactMedium:
+                Feedback.generateImpact(.medium)
+            case .impactHeavy:
+                Feedback.generateImpact(.heavy)
+            }
+        }
+
+        return bag
+    }
+}
---
+++ FlowFeedback.podspec
@@ -0,0 +1,14 @@
+Pod::Spec.new do |s|
+  s.name = 'FlowFeedback'
+  s.version = '1.0.0'
+  s.license = 'MIT'
+  s.summary = 'Generate haptic feedback from Flow signals.'
+  s.homepage = 'https://github.com/HedvigInsurance/FlowFeedback'
+  s.authors = { 'Sam Pettersson' => 'sam@hedvig.com' }
+  s.source = { :git => 'https://github.com/HedvigInsurance/FlowFeedback.git', :tag => '1.0.4' }
+
+  s.ios.deployment_target = '9.0'
+  s.source_files = '*.swift'
+  s.swift_version = '4.2'
+  s.dependency 'FlowFramework', '~> 1.3'
+end
