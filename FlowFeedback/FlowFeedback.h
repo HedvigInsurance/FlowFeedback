bc4d4185401d0c395d9a5a2b77e9686ab487bdeb
Sam Pettersson <sam@sampettersson.com>
2019-01-31 22:06:03 +0100

    Add carthage support

---
+++ FlowFeedback/FlowFeedback.h
@@ -0,0 +1,19 @@
+//
+//  FlowFeedback.h
+//  FlowFeedback
+//
+//  Created by Sam Pettersson on 2019-01-31.
+//  Copyright © 2019 Sam Pettersson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlowFeedback.
+FOUNDATION_EXPORT double FlowFeedbackVersionNumber;
+
+//! Project version string for FlowFeedback.
+FOUNDATION_EXPORT const unsigned char FlowFeedbackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlowFeedback/PublicHeader.h>
+
+
