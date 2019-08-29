93edfb8f24ad4612ab9011a898ba8d54fbf3fc7b
Sam Pettersson <sam@sampettersson.com>
2019-08-29 15:52:39 +0200

    Bump

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FlowFeedback",
-            dependencies: [],
+            dependencies: ["Flow"],
             path: "FlowFeedback"),
     ]
 )
